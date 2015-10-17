d371b7790d1a204a8972aa118d41cfdb5f2f5601
Jeremy Goldberg <elias.goldberg@gmail.com>
2015-10-17 18:36:01 -0400

    updated karma to use firefox instead of chrome because I was getting confused by the multiple chromes running.

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,6 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      'src/main.js'
     ],
 
 
@@ -36,7 +35,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots'],
 
 
     // web server port
@@ -51,14 +50,13 @@ module.exports = function(config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
